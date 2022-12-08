c0dfa475532afc9b08c92f875914363b2e73ad37
Noah Holt <noahryanholt@gmail.com>
2022-12-08 01:38:35 -0600

    now part2 2 isn't working

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -76,6 +76,23 @@ impl Matrix {
         }
     }
 
+    fn scenic_score(&self, i: (usize, usize)) -> Option<u64> {
+        let (row_idx, col_idx) = i;
+        if row_idx >= self.col_length || col_idx >= self.row_length {
+            return None;
+        } else {
+            println!("({}, {})", row_idx, col_idx);
+            let val = self.data[row_idx][col_idx];
+            let l_score = self.left((row_idx, col_idx)).iter().take_while(|x| x <= &&val).count() as u64;
+            let r_score = self.right((row_idx, col_idx)).iter().take_while(|x| x <= &&val).count() as u64;
+            let t_score = self.top((row_idx, col_idx)).iter().take_while(|x| x <= &&val).count() as u64;
+            let b_score = self.bottom((row_idx, col_idx)).iter().take_while(|x| x <= &&val).count() as u64;
+            let score: u64 = l_score * r_score * t_score * b_score;
+            println!("score: {}", score);
+            return Some(score);
+        }
+    }
+
 }
 
 fn part1(s: &String) -> u64 {
@@ -90,11 +107,11 @@ fn part1(s: &String) -> u64 {
     };
     let mut s = 0;
     for (i, x) in m.data.iter().enumerate() {
-        for (j, y) in x.iter().enumerate() {
+        for (j, _) in x.iter().enumerate() {
             match m.is_visible((i, j)) {
                 Some(b) => {
                     if b {
-                        s += *y;
+                        s += 1;
                     }
                 },
                 None => panic!("m.is_visible should always return a bool")
@@ -104,7 +121,34 @@ fn part1(s: &String) -> u64 {
     return s;
 }
 
+fn part2(s: &String) -> u64 {
+    let yeet: Vec<Vec<u64>> = s.split("\n")
+        .map(|x| x.chars().map(|x| x.to_digit(10).expect("should parse char") as u64).collect()).collect();
+    let c = yeet.len();
+    let r = yeet[0].len();
+    let m = Matrix {
+        data: yeet,
+        col_length: c,
+        row_length: r
+    };
+    let mut max = 0;
+    for (i, x) in m.data.iter().enumerate() {
+        for (j, _) in x.iter().enumerate() {
+            match m.scenic_score((i, j)) {
+                Some(b) => {
+                    if b > max {
+                        max = b;
+                    }
+                },
+                None => panic!("m.is_visible should always return a bool")
+            }
+        }
+    }
+    return max;
+}
+
 fn main() -> () {
     let s = String::from(include_str!("../inputs/08_ex.txt"));
     println!("{}", part1(&s));
+    println!("{}", part2(&s));
 }
