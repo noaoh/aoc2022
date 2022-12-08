c26e98f8c1849d18f855b2444fc23253e6dd37bf
Noah Holt <noahryanholt@gmail.com>
2022-12-08 01:10:51 -0600

    currently returning wrong output for example

---
+++ src/bin/08.rs
@@ -0,0 +1,110 @@
+#[derive(Debug)]
+struct Matrix {
+    data: Vec<Vec<u64>>,
+    row_length: usize,
+    col_length: usize
+}
+
+impl Matrix {
+    fn row(&self, n: usize) -> Vec<u64> {
+        let r = self.data[n].clone();
+        return r;
+    }
+
+    fn col(&self, n: usize) -> Vec<u64> {
+        let mut v = Vec::new();
+        for x in &self.data[..] {
+            for (i, y) in x.iter().enumerate() {
+                if i == n {
+                    v.push(*y);
+                }
+            }
+        }
+        return v;
+    }
+
+    fn left(&self, i: (usize, usize)) -> Vec<u64> {
+        let (row_idx, col_idx) = i;
+        let l = self.row(row_idx)[0..col_idx].to_vec();
+        return l;
+    }
+    
+    fn right(&self, i: (usize, usize)) -> Vec<u64> {
+        let (row_idx, col_idx) = i;
+        let r = self.row(row_idx)[col_idx+1..].to_vec();
+        return r;
+    }
+
+    fn top(&self, i: (usize, usize)) -> Vec<u64> {
+        let (row_idx, col_idx) = i;
+        let t = self.col(col_idx)[0..row_idx].to_vec();
+        return t;
+    }
+
+    fn bottom(&self, i: (usize, usize)) -> Vec<u64> {
+        let (row_idx, col_idx) = i;
+        return self.col(col_idx)[row_idx+1..].to_vec();
+    }
+
+    fn is_visible(&self, i: (usize, usize)) -> Option<bool> {
+        let (row_idx, col_idx) = i;
+        if row_idx >= self.col_length || col_idx >= self.row_length {
+            return None;
+        } else {
+            let val = self.data[row_idx][col_idx];
+            let l = self.left((row_idx, col_idx));
+            if l.len() == 0 || l.iter().all(|x| x < &val) {
+                return Some(true);
+            } 
+
+            let r = self.right((row_idx, col_idx));
+            if r.len() == 0 || r.iter().all(|x| x < &val) {
+                return Some(true);
+            }
+
+            let t = self.top((row_idx, col_idx));
+            if t.len() == 0 || t.iter().all(|x| x < &val) {
+                return Some(true);
+            }
+
+            let b = self.bottom((row_idx, col_idx));
+            if b.len() == 0 || b.iter().all(|x| x < &val) {
+                return Some(true);
+            }
+
+            return Some(false);
+        }
+    }
+
+}
+
+fn part1(s: &String) -> u64 {
+    let yeet: Vec<Vec<u64>> = s.split("\n")
+        .map(|x| x.chars().map(|x| x.to_digit(10).expect("should parse char") as u64).collect()).collect();
+    let c = yeet.len();
+    let r = yeet[0].len();
+    let m = Matrix {
+        data: yeet,
+        col_length: c,
+        row_length: r
+    };
+    let mut s = 0;
+    for (i, x) in m.data.iter().enumerate() {
+        for (j, y) in x.iter().enumerate() {
+            match m.is_visible((i, j)) {
+                Some(b) => {
+                    if b {
+                        s += *y;
+                    }
+                },
+                None => panic!("m.is_visible should always return a bool")
+            }
+        }
+    }
+    return s;
+}
+
+fn main() -> () {
+    let s = String::from(include_str!("../inputs/08_ex.txt"));
+    println!("{}", part1(&s));
+}
